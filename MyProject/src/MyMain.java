7893323987b09e6bb1b90037280d62ea1b0808ee
Hp <chetan.singh_da18@gla.ac.in>
2020-02-24 18:30:47 +0530

    initial commit

--- MyProject/src/MyMain.java
+++ MyProject/src/MyMain.java
@@ -67,4 +67,11 @@ public class MyMain {
                     System.out.println("Enter correct choice ------------>");
 
             }
+            System.out.println("Press 1 to add a new contact");
+            System.out.println("Press 2 to view all contacts");
+            System.out.println("Press 3 to search for a contact");
+            System.out.println("Press 4 to delete a contact");
+            System.out.print("Press 5 to exit program:    ");
+            s=sc.nextInt();
+            sc.nextLine();
 }
