669099743ca1fed339386def961577b874ef846e
Hp <chetan.singh_da18@gla.ac.in>
2020-02-24 18:57:36 +0530

    initial commit

--- MyProject/src/MyMain.java
+++ MyProject/src/MyMain.java
@@ -1,5 +1,4 @@
-import Person.Person;
-import mList.mList;
+
 import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
--- MyProject/src/mList.java
+++ MyProject/src/mList.java
@@ -1,3 +1,4 @@
+
 public class mList {
     private Node head;
     public mList(){
