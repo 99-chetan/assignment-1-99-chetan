7ee82a6cb4770300ffc3a2742407a514fe0c011f
Hp <chetan.singh_da18@gla.ac.in>
2020-02-24 17:58:51 +0530

    initial commit

--- MyProject/src/Node.java
+++ MyProject/src/Node.java
@@ -1,5 +1,8 @@
 public class Node {
     private Person data;
     private Node next;
-    public Node(Person data)
+    public Node(Person data){
+        this.data=data;
+        next=null;
+    }
 }
